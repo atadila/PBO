3e69ce35dfcbfbfd8dc6a60c0b86e8d9586366bd
atadila <89756126+atadila@users.noreply.github.com>
2022-08-31 11:29:33 +0700

    Add files via upload

---
+++ MyProject2/src/myproject2/MyProject2.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package myproject2;
+
+/**
+ *
+ * @author lenovo
+ */
+public class MyProject2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("       AA         TTTTTTTTTT        AA");
+        System.out.println("      AA AA           TT           AA AA");
+        System.out.println("     AA   AA          TT          AA   AA");
+        System.out.println("    AAAAAAAAA         TT         AAAAAAAAA");
+        System.out.println("   AA       AA        TT        AA       AA");
+        System.out.println("  AA         AA       TT       AA         AA");
+        System.out.println(" AA           AA      TT      AA           AA");
+        }   
+    }
